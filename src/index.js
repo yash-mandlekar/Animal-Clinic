7778de9637696d58b5ab14373c6c69038f507c9d
yash-mandlekar <ymandlekar9826@gmail.com>
2023-02-05 21:48:50 +0530

    updating

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
-  <App />
+    <App />
   </BrowserRouter>
-  ,document.getElementById('root')
 );
